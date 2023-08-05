39a5eba1a22231ab7a5b85711c3565a3505f3725
Abhishek <abhi.sunrise@gmail.com>
2023-08-05 00:15:05 -0700

    CCON-848-Search-Functionality

---
+++ src/main/java/org/example/Story848.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Story848 {
+
+    public static void main(String[] args) {
+        System.out.println("Add New branch");
+    }
+}
--- src/test/java/demo/ThirdTest.java
+++ src/test/java/demo/ThirdTest.java
@@ -4,5 +4,6 @@ public class ThirdTest {
 
     public static void main(String[] args) {
         System.out.println("Test Third Test class");
+        System.out.println("updated by 848");
     }
 }
